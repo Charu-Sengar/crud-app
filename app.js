a208997e8972be522315eb50174c0a1a156d12b1
Charu-Sengar <charu.sengar@antino.com>
2025-07-09 15:20:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ const port = 3000;
 
 // Create connection to MySQL
 const db = mysql.createConnection({
-    host: 'testdb-1.c34egoce037f.ap-south-1.rds.amazonaws.com',
-    user: 'root',
-    password: '12345678',
-    database: 'testdb_1'
+    host: 'demo-db.cj2uo8wg4n01.ap-south-1.rds.amazonaws.com',
+    user: 'admin',
+    password: '93AhaVwmfQvWWjo1hTA1',
+    database: 'demo_db'
 });
 
 // Connect to MySQL
